4d064bfcb677d1de946b84a0a8a5ad5f7ba8fb4e
moamenashraf1 <133533238+moamenashraf1@users.noreply.github.com>
2023-05-13 22:38:13 +0300

    Add files via upload

---
+++ gpio.d
@@ -0,0 +1,5 @@
+.\gpio.o: GPIO.c
+.\gpio.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\gpio.o: tm4c123gh6pm.h
+.\gpio.o: LCD.h
+.\gpio.o: GPIO.h
---
+++ main.d
@@ -0,0 +1,8 @@
+.\main.o: main.c
+.\main.o: LCD.h
+.\main.o: tm4c123gh6pm.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\main.o: GPS.h
+.\main.o: GPIO.h
